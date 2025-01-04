2af7366941888c9948fb822ce18dc1eaf839c551
GitMinhVu <tominhvuhn@gmail.com>
2025-01-04 12:49:31 +0700

    FE/fix-passwork

--- src/components/UserManagement/InfoManagement.js
+++ src/components/UserManagement/InfoManagement.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import "../../Sass/css/user.css";
-import {Breadcrumb, Card, Tabs, Avatar, Dropdown, Menu, Radio, Space, Form, Input, InputNumber, Button, Upload} from "antd";
+import {Breadcrumb, Card, Tabs, Avatar, Dropdown, Menu, Radio, Space, Form, Input, InputNumber, Button, Upload, message} from "antd";
 import {HomeOutlined, UserOutlined, EditOutlined, EllipsisOutlined, SettingOutlined} from "@ant-design/icons";
 import {useDispatch, useSelector} from "react-redux";
 import {history} from "../../App";
@@ -10,11 +10,20 @@ import {useFormik} from "formik";
 import {PlusOutlined} from "@ant-design/icons";
 import {UpdatelUserAction} from "../../redux/actions/UserAction";
 import {updateUserAvatarAction} from "../../redux/actions/UserAction";
+import {Modal} from "antd";
+import {userService} from "../../redux/services/UserService";
+
 const {TabPane} = Tabs;
 const {Meta} = Card;
 
 export default function InfoManagement(props) {
 	const dispatch = useDispatch();
+	const [isEditModalVisible, setIsEditModalVisible] = useState(false);
+	const [isPasswordModalVisible, setIsPasswordModalVisible] = useState(false);
+	const [passwordForm] = Form.useForm();
+	const [currentPassword, setCurrentPassword] = useState("");
+	const [newPassword, setNewPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
 	// const [avatar, setAvatar] = useState(userLogin?.avatar || "https://storage.googleapis.com/fe-production/images/Auth/account-circle.svg");
 	const {userLogin} = useSelector((state) => state.userReducer);
 	const layout = {
@@ -180,21 +189,119 @@ export default function InfoManagement(props) {
 									<Input onChange={(e) => formik.setFieldValue("phone", e.target.value)} name="phone" value={formik.values.phone} />
 									<p className="text-red-500 text-xs italic mb-0">{formik.errors.phone}</p>
 								</Form.Item>
-								<Form.Item label="Mật Khẩu" rules={[{required: true}]}>
+								{/* <Form.Item label="Mật Khẩu" rules={[{required: true}]}>
 									<Input.Password onChange={(e) => formik.setFieldValue("passWord", e.target.value)} name="passWord" value={formik.values.passWord} />
 									<p className="text-red-500 text-xs italic mb-0">{formik.errors.passWord}</p>
-								</Form.Item>
+								</Form.Item> */}
 								<Form.Item>
-									<Button
-										type="primary"
-										htmlType="submit"
-										onClick={() => {
-											formik.handleSubmit();
-										}}
-									>
-										Cập nhật
-									</Button>
+									<Space>
+										<Button type="primary" onClick={() => setIsEditModalVisible(true)}>
+											Chỉnh sửa thông tin
+										</Button>
+										<Button type="default" onClick={() => setIsPasswordModalVisible(true)}>
+											Đổi mật khẩu
+										</Button>
+									</Space>
 								</Form.Item>
+
+								{/* Modal Chỉnh sửa thông tin */}
+								<Modal
+									title="Chỉnh sửa thông tin"
+									visible={isEditModalVisible}
+									onOk={() => {
+										formik.handleSubmit();
+										setIsEditModalVisible(false);
+									}}
+									onCancel={() => setIsEditModalVisible(false)}
+								>
+									<Form layout="vertical">
+										<Form.Item label="Họ tên">
+											<Input value={formik.values.name} onChange={(e) => formik.setFieldValue("name", e.target.value)} />
+										</Form.Item>
+										<Form.Item label="Email">
+											<Input value={formik.values.email} onChange={(e) => formik.setFieldValue("email", e.target.value)} />
+										</Form.Item>
+										<Form.Item label="Số điện thoại">
+											<Input value={formik.values.phone} onChange={(e) => formik.setFieldValue("phone", e.target.value)} />
+										</Form.Item>
+									</Form>
+								</Modal>
+
+								<Modal
+									title="Đổi mật khẩu"
+									visible={isPasswordModalVisible}
+									onOk={() => {
+										passwordForm.validateFields().then((values) => {
+											if (values.newPassword !== values.confirmPassword) {
+												message.error("Mật khẩu mới không khớp!");
+												return;
+											}
+
+											dispatch(
+												UpdatelUserAction(
+													{
+														...userLogin,
+														password: values.newPassword,
+													},
+													userLogin.id
+												)
+											);
+
+											setIsPasswordModalVisible(false);
+											passwordForm.resetFields();
+											message.success("Đổi mật khẩu thành công!");
+										});
+									}}
+									onCancel={() => {
+										setIsPasswordModalVisible(false);
+										passwordForm.resetFields();
+									}}
+								>
+									<Form form={passwordForm} layout="vertical">
+										<Form.Item
+											label="Mật khẩu hiện tại"
+											name="currentPassword"
+											rules={[
+												{required: true, message: "Vui lòng nhập mật khẩu hiện tại!"},
+												{
+													validator: async (_, value) => {
+														try {
+															const result = await userService.login({
+																email: userLogin.email,
+																password: value,
+															});
+														} catch (error) {
+															throw new Error("Mật khẩu hiện tại không đúng!");
+														}
+													},
+												},
+											]}
+										>
+											<Input.Password />
+										</Form.Item>
+
+										<Form.Item
+											label="Mật khẩu mới"
+											name="newPassword"
+											rules={[
+												{required: true, message: "Vui lòng nhập mật khẩu mới!"},
+												{min: 6, message: "Mật khẩu phải có ít nhất 6 ký tự!"},
+											]}
+										>
+											<Input.Password />
+										</Form.Item>
+										<Form.Item
+											label="Xác nhận mật khẩu mới"
+											name="confirmPassword"
+											rules={[
+												{required: true, message: "Vui lòng xác nhận mật khẩu mới!"},
+												{min: 6, message: "Mật khẩu phải có ít nhất 6 ký tự!"},
+											]}
+										>
+											<Input.Password />
+										</Form.Item>
+									</Form>
+								</Modal>
 							</Form>
 						</div>
 					</div>
